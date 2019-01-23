fb8387e9be2c16a954c2cf424f05b09beccbf72d
Nero27 <nero2twice@gmail.com>
2019-01-22 17:43:25 -0700

    Create screen_title.c

---
+++ screens/screen_title.c
@@ -0,0 +1,55 @@
+#include "raylib.h"
+#include "screens.h"
+
+//----------------------------------------------------------------------------------
+// Global Variables Definition (local to this module)
+//----------------------------------------------------------------------------------
+
+// Title screen global variables
+static int framesCounter;
+static int finishScreen;
+
+//----------------------------------------------------------------------------------
+// Title Screen Functions Definition
+//----------------------------------------------------------------------------------
+
+// Title Screen Initialization logic
+void InitTitleScreen(void)
+{
+    // TODO: Initialize TITLE screen variables here!
+    framesCounter = 0;
+    finishScreen = 0;
+}
+
+// Title Screen Update logic
+void UpdateTitleScreen(void)
+{
+    // TODO: Update TITLE screen variables here!
+
+    if (IsKeyPressed(KEY_ENTER))
+    {
+        //finishScreen = 1;   // OPTIONS
+        finishScreen = 2;   // GAMEPLAY
+    }
+}
+
+// Title Screen Draw logic
+void DrawTitleScreen(void)
+{
+    // TODO: Draw TITLE screen here!
+    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), GREEN);
+    DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
+    DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
+}
+
+// Title Screen Unload logic
+void UnloadTitleScreen(void)
+{
+    // TODO: Unload TITLE screen variables here!
+}
+
+// Title Screen should finish?
+int FinishTitleScreen(void)
+{
+    return finishScreen;
+}
